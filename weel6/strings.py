1882dcf3eef93ecf09841b309e3378c0cbb610fd
Vishnuvardhan1567 <babbluvarma1567@gmail.com>
2025-05-01 12:06:33 +0530

    Create strings.py

---
+++ weel6/strings.py
@@ -0,0 +1,34 @@
+my_string = "VISHNU,VARDHAN"
+print(my_string[0:6])
+print(my_string[7:])
+print(my_string[:6])
+print(my_string[-7:])
+my_string = "vishnuvardhan"
+length = len(my_string)
+print(length)
+string1="VISHNU"
+string2="VARDHAN"
+concatenated_string = string1 + ' ' + string2
+print(concatenated_string)
+text = "PYTHON LAB"
+print(text.lower())
+text = "python lab"
+print(text.upper())
+text = "  ACE CLG  "
+print(text.strip())
+text = "COUMPUTER LAB"
+print(text.split())
+text = "COUMPUTER,LAB,EXAM"
+print(text.split(","))
+text = "vishnu vardhan"
+print(text.replace("vardhan","varma"))
+text = "babblu varma"
+print(text.find("varma"))
+print(text.find("harsha"))
+my_string = "Fire,FOx!"
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.strip())
+print(my_string.split(','))
+print(my_string.replace("Fox","Box"))
+print(my_string.find("Fire"))
